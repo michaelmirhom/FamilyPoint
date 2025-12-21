84eeb3d9d1a741cbdafb0cf73c2562fb11a6ee70
michaelmirhom <micharby73@gmail.com>
2025-12-21 00:13:35 -0600

    Allow all CORS for easy deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ origins = os.getenv(
 ).split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
